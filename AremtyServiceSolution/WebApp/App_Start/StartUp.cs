cfcebace2cc71fa73f042c91c8df10204a08a13e
Aleksey Vyaznikov <alexv47@mail.ru>
2017-08-22 14:08:00 +0300

    a

---
+++ AremtyServiceSolution/WebApp/App_Start/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using Owin;
+using WebApp.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using WebApp.Models.Identity;
+
+namespace WebApp
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<AppIdenityDbContext>(AppIdenityDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
