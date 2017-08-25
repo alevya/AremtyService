39b8e845cc39509019cb36d155e96ad8d7c706eb
alevya <alexv47@mail.ru>
2017-08-25 22:55:15 +0300

    a

---
+++ AremtyServiceSolution/AremtyServiceWeb/Models/Aremty/AremtyContextDb.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+
+namespace WebApp.Models.Aremty
+{
+    public class AremtyContextDb
+    {
+        #region Init
+
+        public AremtyContextDb()
+        { }
+
+        public DbSet<GuiDashboard> GuiDashboards { get; set; }
+        public DbSet<GuiPanel> GuiPanels { get; set; }
+        public DbSet<Widget> Widgets { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ AremtyServiceSolution/AremtyServiceWeb/Models/Aremty/Widget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApp.Models.Aremty
+{
+    public class Widget
+    {
+        public Guid WidgetId { get; set; }
+        public Guid GuiPanelId { get; set; }
+        public string DisplayName { get; set; }
+        public string Alias { get; set; }
+        public int OrderSort { get; set; }
+    }
+}
